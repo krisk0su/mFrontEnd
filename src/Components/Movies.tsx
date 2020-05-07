4c736dceb48643deafa86e82214cfb9ac0210697
chrizkonf <chrizkoof@gmail.com>
2020-05-07 20:43:21 +0300

    create movies implemented... get all movies also

--- src/Components/Movies.tsx
+++ src/Components/Movies.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export const Movies = () => {
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    // Update the document title using the browser API
+    axios
+      .get("http://localhost:3000/movies/all")
+      .then((res: any) => console.log("res", res));
+  });
   return <h1>Here will be displayd all the movies...</h1>;
 };
