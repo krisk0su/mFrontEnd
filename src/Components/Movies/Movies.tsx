d2a7acd194a2c11f17f6fd56be886de0c2bcefaf
Christian <chrizkoof@gmail.com>
2020-05-14 23:00:29 +0300

    genre

--- src/Components/Movies/Movies.tsx
+++ src/Components/Movies/Movies.tsx
@@ -1,26 +1,68 @@
 import React, { useState, useEffect, Fragment } from "react";
 import axios from "axios";
 import { Movie } from "./Movie";
-import { Card, Icon, Image } from "semantic-ui-react";
+import { Card, Icon, Image, Dropdown, Segment, Button } from "semantic-ui-react";
 import { IMovie } from "../Interfaces/IMovie";
+import { IGenre } from "../Interfaces/IGenre";
 
 export const Movies = () => {
   const [movies, setMovies] = useState([]);
-
+  const [genreOptions, setGenreOptions] = useState([]);
+  const [genre, setGenre] = useState();
   const fetchData = async () => {
-    const moviesRes = await axios("http://localhost:3000/movies/all");
+    const moviesRes = await axios("http://localhost:3000/movies/all", {
+      params: {
+        genre
+      }
+    });
+    const genresRes = await axios("http://localhost:3000/genres");
+
+    const genres = genresRes.data.map((genre: IGenre) => ({
+      key: genre._id,
+      value: genre._id,
+      text: genre.name
+    }))
+    setGenreOptions(genres);
     setMovies(moviesRes.data);
   };
+  const onGenreSelect = (e: any, value: any) => {
+    setGenre(value.value);
+  }
+  const movieByGenre = async () => {
+    if (genre) {
+      const res = await axios.post("http://localhost:3000/movies/all", {
+        params: {
+          genre
+        }
+      });
+      console.log("res", res)
+    }
+  }
   useEffect(() => {
     fetchData();
   }, []);
   const displayMovies = () => {
     return (
-      <Card.Group itemsPerRow={8}>
-        {movies.map((mov: IMovie) => (
-          <Movie Movie={mov} />
-        ))}
-      </Card.Group>
+      <div>
+        <Segment>
+          <Dropdown
+            placeholder='Select Genre'
+            search
+            selection
+            options={genreOptions}
+            onChange={onGenreSelect}
+          />
+          <Button primary onClick={movieByGenre}>Search by genre</Button>
+        </Segment>
+        <Segment>
+          <Card.Group itemsPerRow={8} >
+            {movies.map((mov: IMovie) => (
+              <Movie Movie={mov} />
+            ))}
+          </Card.Group>
+        </Segment>
+      </div>
+
     );
   };
   return <Fragment>{movies && displayMovies()}</Fragment>;
