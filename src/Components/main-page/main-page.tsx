4996bc82a1471398b214c01d86d9ca16548b3843
Bojidar Tonev <ghost1993ifyy@gmail.com>
2020-08-24 01:29:28 +0300

    SASSSSSSSSSSSSSSSSSSSSSSSSSSSSSS

--- src/Components/main-page/main-page.tsx
+++ src/Components/main-page/main-page.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Movie } from "../movie/movie";
-import { MovieRow } from "../movie-row/movie-row"
+import { MovieRow } from "../movie-row/movie-row";
 import "./main-page.scss";
 
 export const MainPage = () => {
   return (
     <div className="main">
-      <MovieRow title="CURRENTLY TRENDING"/>
-      <MovieRow title="RECENT RELEASES"/>
-      <MovieRow title="DOCUMENTARIES"/>
-      <MovieRow title="COMEDIES"/>
+      <MovieRow title="CURRENTLY TRENDING" cls="trending" />
+      <MovieRow title="RECENT RELEASES" cls="recent" />
+      <MovieRow title="DOCUMENTARIES" cls="documentaries" />
+      <MovieRow title="COMEDIES" cls="comedies" />
     </div>
   );
 };
--- src/Components/movie-row/movie-row.tsx
+++ src/Components/movie-row/movie-row.tsx
@@ -3,9 +3,9 @@ import { Movie } from "../movie/movie";
 import "./movie-row.scss";
 
 export const MovieRow = (props: any) => {
-  const { movies, title } = props;
+  const { movies, title, cls } = props;
   return (
-    <div className="movie-row">
+    <div className={`movie-row ${cls}`}>
       <div className="title">
         <hr />
         <h2>{title}</h2>
