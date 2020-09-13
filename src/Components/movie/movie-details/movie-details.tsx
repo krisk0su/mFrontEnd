3947241fad670d88516a10e4c07fdd08aaac2403
Bojidar Tonev <ghost1993ifyy@gmail.com>
2020-09-14 00:59:02 +0300

    mixins'n stuff

--- src/Components/movie/movie-details/movie-details.tsx
+++ src/Components/movie/movie-details/movie-details.tsx
@@ -11,16 +11,14 @@ export const MovieDetails = (props: any) => {
         alt=""
       />
       <div className="movie-details-header">
-        <div className="movie-details-header-information">
-          <div className="start-watching">
-            <i className="fas fa-play"></i>
-            <span>Watch latest episode</span>
-          </div>
-          <div className="movie-interact">
-            <i className="fas fa-share-alt"></i>
-            <i className="fas fa-plus"></i>
-            <i className="fas fa-heart"></i>
-          </div>
+        <div className="start-watching">
+          <i className="fas fa-play-circle"></i>
+          <div>Watch latest episode</div>
+        </div>
+        <div className="movie-interact">
+          <i className="fas fa-share-alt"></i>
+          <i className="fas fa-plus"></i>
+          <i className="fas fa-heart"></i>
         </div>
       </div>
     </div>
